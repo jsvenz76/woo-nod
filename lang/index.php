06a0be74e921e269b3c2c6d1da9b3099f8bb4918
mikeyhoward1977 <mike@mikeandniki.co.uk>
2016-02-11 22:49:55 +0000

    Updating assets

--- lang/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	defined( 'ABSPATH' ) or die( "Direct access to this page is disabled!!!" );
-/**
- * This is a blank placeholder file to ensure the directory it resides in is created
- * regardless of whether or not it is empty.
- *
- *
- */	
-?>
